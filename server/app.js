cf1592414d3e5a1428e859555fdd31d3bdd82056
Alex Jung <alexandermjung89@gmail.com>
2017-08-01 16:47:18 -0500

    added random number function

--- server/app.js
+++ server/app.js
@@ -4,13 +4,20 @@ var app = express();
 
 port = 5000;
 
+
+app.use(express.static('server/public'))
+
 app.get('/*', function(req, res){
     res.send('request recieved');
 });
 
-app.use(express.static('public'))
+app.get('/random', function(req, res){ 
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}});
+
 
 app.listen(port, function(){
     console.log('listening on port', port);
-});
+});//end of listening
 
